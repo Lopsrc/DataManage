2ba95e74b9bff50229d6f9cae4fa75254972f242
Lopsrc <96688905+Lopsrc@users.noreply.github.com>
2023-06-22 22:38:52 +0300

    cli

--- client/cmd/main.py
+++ client/cmd/main.py
@@ -1,105 +1,119 @@
 import dbWork
 from tabulate import tabulate 
-import json
 
-fix_price_of_the_day = 3200
-TOKEN = 'J/79d4=Yutb7J!YYgMBvsH12PyxAa197v1CTkWkv4QBy?T4UHDUEiexn0t1HVBNAQ-9429md/8!hmkFZOV!9oeyGbwo0q0mDUYEa7cPloIFu8DjDLj=eKQoOQONPKywwOv?MQtv!rkNWVoNUEv2sTwY3HOxeUUBHeOtXD-voZ12vD3pOZQm6VcspJa7jhuCloAx-unzh?0gXXMVVGsjMZc=eKH2LG!5SOEQ3Xy8BxlccLACoHRB2Df-njeMbJ79a'
+TOKEN = 'vYq?VQ?Gvg==9cow9neCUCwc3YpoA5kmOBrdQL!/9k60iNjr3i6?RS?fKr3UDEkbNUKKdt16xhio3yJxj8-1Ql84BUmDZ4fQq5CpeyNaF?!5qiJvjfd5a?OEP0KMYo2vt/yTxpv14hfpDs768?8Et5cZzxJgd2TlkBtKzvj0pXsRzW-95iwWREFwp7wCqzOD1XRg553okTUJKpAQr7wulPLln?fMIrBid9b5x!9oCENq6Z=FtaaiNECuc5?Q6cRi'
 
-def count_price_of_the_day(time_work, penalty):
-    if time_work <= 4:
-        return int(fix_price_of_the_day/2-penalty)
-    return int(fix_price_of_the_day-penalty)
+def count_price_of_the_day(penalty, price):
+    return int(price-penalty)
 
 def printTable(data):
     table = tabulate(data, headers="keys", tablefmt="psql")
     print(table)
 
+def createRecordFromWorkspace(token):
+    nameworkspace = str(input("Enter the name of the workplace: "))
+    date = str(input("Enter the date(yyyy-mm-dd): "))
+    time_work = int(input("Enter the working time(8 or 4): "))
+    price = int(input("Enter the price(3200 or other price): "))
+    penalty = int(input("Enter the penalty(0 or other num): "))
 
+    price = count_price_of_the_day(penalty, price)
+
+    response = dbWork.createRecordToWorkspace(nameworkspace, date, price, time_work, penalty, token)
+    
+    if response.status_code == 200:
+        print('Запись успешно создана')
+    else:
+        print('Ошибка при создании записи:', response.text, response.status_code)
+def updateRecordFromWorkspace(token):
+    record_id = int(input("Enter id of the record: "))
+    nameworkspace = str(input("Enter the name of the workplace: "))
+    date = str(input("Enter the date(yyyy-mm-dd): "))
+    time_work = int(input("Enter the working time(8 or 4): "))
+    price = int(input("Enter the price(3200 or other price): "))
+    penalty = int(input("Enter the penalty(0 or other num): "))
+    price = count_price_of_the_day(penalty, price)
+    
+    response = dbWork.updateRecordToWorkspace(record_id,nameworkspace, date, price, time_work, penalty, token)
+    if response.status_code == 200:
+        print('Запись успешно создана')
+    else:
+        print('Ошибка при создании записи:', response.text, response.status_code)
+def deleteRecordFromWorkspace(token):
+    record_id = int(input("Enter id of the record: "))  # Идентификатор записи, которую нужно удалить
+    response = dbWork.deleteRecordToWorkspace(record_id, token)
+    if response.status_code == 200:
+        print('Запись успешно удалена')
+    else:
+        print('Ошибка при удалении записи:', response.text)
+def getAllRecordsFromWorkspace(token):
+    response = dbWork.getAllRecordsToWorkspace(token)
+    if response.status_code == 200:
+        print('Запись успешно получена')
+    else:
+        print('Ошибка при получении записи:', response.text)
+        return
+    printTable(response.json())
+
+def createRecordFromListPayments(token):
+    date          = str(input("Enter date(yyyy-mm-dd): "))
+    nameworkspace = str(input("Enter the name of the workplace: "))
+    price         = int(input("Enter price: "))
+
+    response = dbWork.createRecordToListPayments(nameworkspace, date, price, token)
+    if response.status_code == 200:
+        print('Запись успешно создана')
+    else:
+        print('Ошибка при создании записи:', response.text, response.status_code)
+def updateRecordFromListPayments(token):
+    record_id     = int(input("Enter id of the record: "))
+    nameworkspace = str(input("Enter the name of the workplace: "))
+    date          = str(input("Enter date(yyyy-mm-dd): "))
+    price         = int(input("Enter price: "))
+
+    response = dbWork.updateRecordToListPayments( record_id, nameworkspace, date, price, token)
+    if response.status_code == 200:
+        print('Запись успешно создана')
+    else:
+        print('Ошибка при создании записи:', response.text, response.status_code)
+def deleteRecordFromListPayments(token):
+    record_id = int(input("Enter id of the record: ")) # Идентификатор записи, которую нужно удалить
+
+    response = dbWork.deleteRecordToListPayments( record_id, token)
+    if response.status_code == 200:
+        print('Запись успешно удалена')
+    else:
+        print('Ошибка при удалении записи:', response.text)
+def getAllRecordsFromListPayments(token):
+    response = dbWork.getAllRecordsToListPayments(token)
+    if response.status_code == 200:
+        print('Запись успешно получена')
+    else:
+        print('Ошибка при получении записи:', response.text)
+        return
+    printTable(response.json())
+    
 def workspaceTable(token_):
     action = int(input("Select action (1. Create new record, 2. Update record, 3. Delete record, 4. Get all records): "))
     if action==1:
-        nameworkspace = str(input("Enter the name of the workplace: "))
-        date = str(input("Enter the date(yyyy-mm-dd): "))
-        time_work = int(input("Enter the working time(8 or 4): "))
-        penalty = int(input("Enter the penalty(0 or other num): "))
-        price = count_price_of_the_day(time_work, penalty)
-
-        response = dbWork.createRecordToWorkspace(nameworkspace, date, price, time_work, penalty, token_)
-        if response.status_code == 200:
-            print('Запись успешно создана')
-        else:
-            print('Ошибка при создании записи:', response.text, response.status_code)
+        createRecordFromWorkspace(token_)
     elif action==2:
-        record_id = int(input("Enter id of the record: "))
-        nameworkspace = str(input("Enter the name of the workplace: "))
-
-        date = str(input("Enter date(yyyy-mm-dd): "))
-        time_work = int(input("Enter time of work(8 or 4): "))
-        penalty = int(input("Enter penalty(0 or other num): "))
-        price = count_price_of_the_day(time_work, penalty)
-        
-        response = dbWork.updateRecordToWorkspace(record_id,nameworkspace, date, price, time_work, penalty, token_)
-        if response.status_code == 200:
-            print('Запись успешно создана')
-        else:
-            print('Ошибка при создании записи:', response.text, response.status_code)
+        updateRecordFromWorkspace(token_)
     elif action==3:
-        record_id = int(input("Enter id of the record: "))  # Идентификатор записи, которую нужно удалить
-
-        response = dbWork.deleteRecordToWorkspace(record_id, token_)
-        if response.status_code == 200:
-            print('Запись успешно удалена')
-        else:
-            print('Ошибка при удалении записи:', response.text)
+        deleteRecordFromWorkspace(token_)
     elif action==4:
-        response = dbWork.getAllRecordsToWorkspace(token_)
-        if response.status_code == 200:
-            print('Запись успешно получена')
-        else:
-            print('Ошибка при получении записи:', response.text)
-            return
-        printTable(response.json())
-
+        getAllRecordsFromWorkspace(token_)
 
 def listPaymentsTable(token_):
     action = int(input("Select action (1. Create new record, 2. Update record, 3. Delete record, 4. Get all records): "))
     if action==1:
-        date = str(input("Enter date(yyyy-mm-dd): "))
-        nameworkspace = str(input("Enter the name of the workplace: "))
-        price = int(input("Enter price of the day(3200): "))
-
-        response = dbWork.createRecordToListPayments(nameworkspace, date, price, token_)
-        if response.status_code == 200:
-            print('Запись успешно создана')
-        else:
-            print('Ошибка при создании записи:', response.text, response.status_code)
+        createRecordFromListPayments(token_)
     elif action==2:
-        record_id = int(input("Enter id of the record: "))
-        nameworkspace = str(input("Enter the name of the workplace: "))
-        date = str(input("Enter date(yyyy-mm-dd): "))
-        price = int(input("Enter price of the day(3200): "))
-
-        response = dbWork.updateRecordToListPayments( record_id, nameworkspace, date, price, token_)
-        if response.status_code == 200:
-            print('Запись успешно создана')
-        else:
-            print('Ошибка при создании записи:', response.text, response.status_code)
+        updateRecordFromListPayments(token_)
     elif action==3:
-        record_id = int(input("Enter id of the record: ")) # Идентификатор записи, которую нужно удалить
-
-        response = dbWork.deleteRecordToListPayments( record_id, token_)
-        if response.status_code == 200:
-            print('Запись успешно удалена')
-        else:
-            print('Ошибка при удалении записи:', response.text)
+        deleteRecordFromListPayments(token_)
     elif action==4:
-        response = dbWork.getAllRecordsToListPayments(token_)
-        if response.status_code == 200:
-            print('Запись успешно получена')
-        else:
-            print('Ошибка при получении записи:', response.text)
-            return
-        printTable(response.json())
+        getAllRecordsFromListPayments(token_)
 
 
 if __name__=="__main__":
